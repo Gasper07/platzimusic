1be45fff31b4585fe16e80b4c64dbdbaf80d2803
edgardo josue flores barrera <freelancer@edgardofloresdvlp.com>
2018-08-19 14:36:29 -0600

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+	appname: 'platzimusic07',
+	apiKey: '63d48ba1ad8c99ae09394451299e179f',
+	secret: '03212d0b0d1782ea93f3ad6ac3039fcc',
+	registeredTo: 'Gasper07'
+}
+
+
+// Application name	platzimusic07
+// API key	63d48ba1ad8c99ae09394451299e179f
+// Shared secret	03212d0b0d1782ea93f3ad6ac3039fcc
+// Registered to	Gasper07
